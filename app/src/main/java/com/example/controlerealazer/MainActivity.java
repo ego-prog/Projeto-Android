2addde85680ef8bdb248c6a04e583651852c45da
ego-prog <ego@ego.adv.br>
2023-03-29 14:30:37 -0300

    splash

--- app/src/main/java/com/example/controlerealazer/MainActivity.java
+++ app/src/main/java/com/example/controlerealazer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.controlerealazer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Looper;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      /*  Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
+                },3000)*/
     }
 }
