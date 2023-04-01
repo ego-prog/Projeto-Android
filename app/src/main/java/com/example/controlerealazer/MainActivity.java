45bff7e3e026ed5ec2b8a66eeaba25359830f2ef
ego-prog <ego@ego.adv.br>
2023-03-31 22:11:05 -0300

    splash screen

--- app/src/main/java/com/example/controlerealazer/MainActivity.java
+++ app/src/main/java/com/example/controlerealazer/MainActivity.java
@@ -2,21 +2,25 @@ package com.example.controlerealazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 
-import java.util.logging.Handler;
-
 public class MainActivity extends AppCompatActivity {
-
+private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      /*  Handler(Looper.getMainLooper()).postDelayed({
-                val intent = Intent(this, FormLogin::class.java)
-        startActivity(intent)
-        finish()
-                },3000)*/
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
