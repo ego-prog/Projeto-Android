dd078b2ed7f9bd1c207cc04aca32dd5ce433fa96
ego-prog <ego@ego.adv.br>
2023-04-05 15:36:02 -0300

    Implementação Cadastrar

--- app/src/main/java/com/example/controlerealazer/FormCadastro.java
+++ app/src/main/java/com/example/controlerealazer/FormCadastro.java
@@ -1,5 +1,6 @@
 package com.example.controlerealazer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
@@ -8,12 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormCadastro extends AppCompatActivity {
-   private EditText edit_nome,edit_email, edit_senha;
-   private Button bt_cadastrar;
-   String[] mensagens = {"Preencha todos os dados", "Cadastro realizado com sucesso"};
+    private EditText edit_nome, edit_email, edit_senha;
+    private Button bt_cadastrar;
+    String[] mensagens = {"Preencha todos os dados", "Cadastro realizado com sucesso", "Erro ao Cadastrar"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +32,44 @@ public class FormCadastro extends AppCompatActivity {
                 String nome = edit_nome.getText().toString();
                 String email = edit_email.getText().toString();
                 String senha = edit_senha.getText().toString();
-                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
-                    Snackbar snackbar = Snackbar.make(view,mensagens[0], Snackbar.LENGTH_SHORT);
-                    snackbar.setBackgroundTint(Color.WHITE);
-                    snackbar.setTextColor(Color.BLACK);
-                    snackbar.show();
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                    Alerta(view, mensagens[0]);
 
-                }else{
+                } else {
+                    CadastrarUsuario(view, email, senha);
 
                 }
 
             }
         });
     }
-    private void IniciarComponentes(){
+
+    private void CadastrarUsuario(View view, String email, String senha) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Alerta(view, mensagens[1]);
+                } else {
+                    Alerta(view, mensagens[2]);
+                }
+
+            }
+        });
+    }
+
+    private void IniciarComponentes() {
         edit_nome = findViewById(R.id.edit_nome);
         edit_email = findViewById(R.id.edit_email);
         edit_senha = findViewById(R.id.edit_senha);
         bt_cadastrar = findViewById(R.id.bt_cadastrar);
+    }
+
+    private void Alerta(View view, String mensagem) {
+        Snackbar snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT);
+        snackbar.setBackgroundTint(Color.WHITE);
+        snackbar.setTextColor(Color.BLACK);
+        snackbar.show();
+
     }
 }
