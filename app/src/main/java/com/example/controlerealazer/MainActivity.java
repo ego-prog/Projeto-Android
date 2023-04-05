b4411a6fe5e2b7befe6123779ff05c488e5d6dad
ego-prog <ego@ego.adv.br>
2023-04-05 18:38:19 -0300

    Update

--- app/src/main/java/com/example/controlerealazer/MainActivity.java
+++ app/src/main/java/com/example/controlerealazer/MainActivity.java
@@ -1,26 +1,24 @@
 package com.example.controlerealazer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-private static int SPLASH_TIME_OUT = 3000;
+private static final int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,FormLogin.class);
-                startActivity(intent);
-                finish();
-            }
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this,FormLogin.class);
+            startActivity(intent);
+            finish();
         },SPLASH_TIME_OUT);
     }
 }
