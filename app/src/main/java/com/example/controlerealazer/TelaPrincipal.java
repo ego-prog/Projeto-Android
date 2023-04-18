4633c051145f6271abe87bf4653516c326860701
ego-prog <ego@ego.adv.br>
2023-04-18 10:31:10 -0300

    Exclusão

--- app/src/main/java/com/example/controlerealazer/TelaPrincipal.java
+++ app/src/main/java/com/example/controlerealazer/TelaPrincipal.java
@@ -2,25 +2,34 @@ package com.example.controlerealazer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class TelaPrincipal extends AppCompatActivity {
     private TextView nomeUsuario, emailUsuario;
-    private Button bt_deslogar;
+    private Button bt_deslogar, bt_excluir;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String usuarioID;
 
@@ -35,6 +44,17 @@ public class TelaPrincipal extends AppCompatActivity {
         bt_deslogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        bt_excluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                excluirCadastroUsuario();
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(TelaPrincipal.this, FormLogin.class);
                 startActivity(intent);
@@ -66,5 +86,43 @@ public class TelaPrincipal extends AppCompatActivity {
         nomeUsuario = findViewById(R.id.textNomeUsuario);
         emailUsuario = findViewById(R.id.textEmailUsuario);
         bt_deslogar = findViewById(R.id.bt_deslogar);
+        bt_excluir = findViewById(R.id.bt_excluir);
+    }
+
+    private void excluirCadastroUsuario() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("db", "Sucesso ao excluir os dados");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("db_erro", "Erro ao excluir os dados" + e.toString());
+                    }
+                });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("db", "Sucesso ao excluir Usuário");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("db_erro", "Erro ao excluir Usuário" + e.toString());
+                    }
+                });
+
     }
 }
